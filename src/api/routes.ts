dfb5f917176e49a32ae672da729d901be70f4db3
trikitrok <trikitrok@gmail.com>
2025-06-15 02:12:44 +0200

    fix error: this was undefined

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -4,6 +4,6 @@ import {RunAnalysis} from "../domain/runAnalysis";
 
 const router = Router();
 
-router.get('/v1/analysis', new AnalysisController(new RunAnalysis()).analyze);
+router.get('/v1/analysis', (req, res) => new AnalysisController(new RunAnalysis()).analyze(req, res));
 
 export default router;
